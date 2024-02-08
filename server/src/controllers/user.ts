45ebb258e833689566b33b3c3146be518386244d
sriman vikram <srimanvgn@gmail.com>
2024-02-08 10:55:26 +0530

    feat : feat: 🦜🔗 Memory integration Changes: - Added Conversational buffer memory to persist Human Ai interaction - Initiated llama2 model using Ollama - renamed openai.ts to models.ts - installed types for langchain/core
    Fixes:
    - removed nodemon from root and installed as dev dependency for server

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -2,7 +2,10 @@ import { OpenAIClient, toFile } from "@langchain/openai";
 import { Request, Response } from "express";
 import fs from "fs";
 import { pc } from "../lib/pinecone.js";
-import { embedding } from "../lib/openai.js";
+import { memory, embedding, ollama } from "../lib/models.js";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { LLMChain } from "langchain/chains";
+import { ConversationChain } from "langchain/chains";
 
 export const suggestion = async (req: Request, res: Response) => {
   const filePath = "shirt.mp3";
@@ -18,6 +21,27 @@ export const suggestion = async (req: Request, res: Response) => {
     const transcript = response.text;
 
     if (!transcript.trim) throw new Error("No transcript found");
+
+    const prompt = new PromptTemplate({
+      inputVariables: ["history", "input"],
+      template: `summarize what the user is trying to buy in a single statement. get human conversation from Previous conversation section.don't say anything extra.
+         
+          {history}
+
+          Input :
+          {input}
+        `,
+    });
+    const chain = new ConversationChain({
+      llm: ollama,
+      memory: memory,
+      prompt: prompt,
+    });
+
+    const resData: string = await req.body.input;
+
+    const data = await chain.invoke({ input: resData });
+    console.log(memory.chatHistory);
 
     const index = pc.Index(process.env.PINECONE_INDEX!);
 
